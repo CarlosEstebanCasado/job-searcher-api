29bf5fadfdba083bb30d3e393809f63b0128874c
Carlos Esteban <carlosbcn169@gmail.com>
2020-08-26 02:54:12 +0200

    Some changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
-const port = 3001;
+
+app.use(cors())
 
 var redis = require("redis");
 var client = redis.createClient(process.env.REDIS_URL);
@@ -16,5 +18,5 @@ app.get('/jobs', async(req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  //console.log(`Example app listening at http://localhost:${port}`)
 })
